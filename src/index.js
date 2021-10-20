5dfa479b3371b4b7425dd1e7830cfba2d1ba86a1
clxxiii <earthnoartisjusteh@gmail.com>
2021-10-20 10:17:23 -0400

    user-by-user caching

--- src/index.js
+++ src/index.js
@@ -10,8 +10,9 @@ const API_URL = "https://7yortti0f2.execute-api.us-east-2.amazonaws.com/api?user
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const callTime = new Date().getTime();
-let testjson = {"success":true,"data":{"user":{"_id":"6076577db88e696ebeb54ba7","username":"themonkifier","role":"user","ts":"2021-04-14T02:46:21.716Z","badges":[],"xp":1086107,"gamesplayed":469,"gameswon":206,"gametime":202970.15600000013,"country":"US","supporter_tier":0,"verified":false,"league":{"gamesplayed":282,"gameswon":154,"rating":21448.868415521607,"glicko":1971.020337900401,"rd":62.39632916600284,"rank":"ss","apm":43.17,"pps":1.36,"vs":90.48,"decaying":false,"standing":3455,"percentile":0.10830302270161796,"standing_local":774,"prev_rank":"s+","prev_at":3508,"next_rank":"u","next_at":1595,"percentile_rank":"ss"},"avatar_revision":1621951855974,"friend_count":5}},"cache":{"status":"miss","cached_at":1634736913287,"cached_until":1634737213287}}
+let testjson = {"success":true,"data":{"user":{"_id":"5e7783c830fb9028f9944a28","username":"btmc","role":"user","ts":"2020-03-22T15:27:04.304Z","badges":[{"id":"infdev","label":"Participated as tester in the INFDEV development phase"},{"id":"secretgrade","label":"Achieved the full Secret Grade","ts":"2020-05-04T17:23:00.939Z"},{"id":"kod_founder","label":"KO'd the founder of TETR.IO","ts":"2020-08-18T17:51:06.287Z"},{"id":"early-supporter","label":"Early Supporter"},{"id":"100player","label":"Emerged victorious in a 100+ player game","ts":"2021-04-01T17:56:51.937Z"}],"xp":946171,"gamesplayed":875,"gameswon":249,"gametime":262298.74007055536,"country":"IM","supporter":true,"supporter_tier":3,"verified":true,"league":{"gamesplayed":349,"gameswon":180,"rating":20625.26305573843,"glicko":1906.3737332751252,"rd":93.47929579936012,"rank":"s+","apm":37.19,"pps":1.25,"vs":80.13,"decaying":true,"standing":4345,"percentile":0.1361286076901382,"standing_local":7,"prev_rank":"s","prev_at":5424,"next_rank":"ss","next_at":3511,"percentile_rank":"s+"},"friend_count":136}},"cache":{"status":"miss","cached_at":1634738990483,"cached_until":1634739290483}}
 
+let user = urlParams.get("user");
 const rankColors = {
   "d": "#856C84",
   "d+": "#815880",
@@ -35,10 +36,9 @@ const rankColors = {
 
 async function getUserData() {
   try {
-    let currentJson = JSON.parse(localStorage.getItem("tetrioStatsUserData"))
+    let currentJson = JSON.parse(localStorage.getItem("tetrioStatsUserData-" + user))
     // Set cache time to 0 if no call has been stored yet
     if (currentJson == null) currentJson = { "data":{"user":{"username": 0}}, "cache":{"cached_until": 0} }
-    let user = urlParams.get("user");
     // Only make a call if the current data is outdated
     console.log("Caching Information: " + currentJson.cache.cached_until + " > " + callTime)
     if (currentJson.data.user.username == user && currentJson.cache.cached_until > callTime) { console.log("using cached data"); return currentJson  }
@@ -52,19 +52,19 @@ async function getUserData() {
       }
       let response = await fetch(API_URL + user, params)
       response = await response.json();
-      localStorage.setItem("tetrioStatsUserData", JSON.stringify(response));
+      localStorage.setItem("tetrioStatsUserData-" + user, JSON.stringify(response));
 
       return response;
       }
     }
     catch (error) {
-      localStorage.removeItem("tetrioStatsUserData");
+      localStorage.removeItem("tetrioStatsUserData-" + user);
     }
   }
 
 async function updateData() {
-  let json = await getUserData();
-  // let json = testjson;
+  // let json = await getUserData();
+  let json = testjson;
   try {
     console.log(json)
     nameRankTR.style.filter = "blur(0px)";
@@ -80,10 +80,10 @@ async function updateData() {
     globalRank.innerHTML = "#" + json.data.user.league.standing;
   }
   catch (error) {
-    localStorage.removeItem("tetrioStatsUserData");
+    localStorage.removeItem("tetrioStatsUserData-" + user);
   }
 }
 
 function logData() {
-  console.log(localStorage.getItem("tetrioStatsUserData"))
+  console.log(localStorage.getItem("tetrioStatsUserData-" + user))
 }
