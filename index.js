806194e2d592e807b8f413e0ee19f95dc3f0b338
clxxiii <earthnoartisjusteh@gmail.com>
2021-10-20 01:38:13 -0400

    removed axios don't need it

--- index.js
+++ index.js
@@ -5,62 +5,6 @@ const API_URL = "https://7yortti0f2.execute-api.us-east-2.amazonaws.com/api";
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const callTime = new Date().getTime();
-const testJson = {
-   "success":true,
-   "data":{
-      "user":{
-         "_id":"5e7a392fe55cb634a81b6127",
-         "username":"clxxiii",
-         "role":"user",
-         "ts":"2020-03-24T16:45:35.429Z",
-         "badges":[
-            {
-               "id":"secretgrade",
-               "label":"Achieved the full Secret Grade",
-               "ts":"2020-05-12T18:33:53.123Z"
-            }
-         ],
-         "xp":2093104,
-         "gamesplayed":1865,
-         "gameswon":537,
-         "gametime":644170.140795555,
-         "country":"US",
-         "supporter":true,
-         "supporter_tier":1,
-         "verified":false,
-         "league":{
-            "gamesplayed":504,
-            "gameswon":257,
-            "rating":20296.512490516423,
-            "glicko":1883.4759423564303,
-            "rd":67.18081458528094,
-            "rank":"s+",
-            "apm":33.15,
-            "pps":1.43,
-            "vs":76.31,
-            "decaying":false,
-            "standing":4638,
-            "percentile":0.15050308341447582,
-            "standing_local":1012,
-            "prev_rank":"s",
-            "prev_at":5237,
-            "next_rank":"ss",
-            "next_at":3390,
-            "percentile_rank":"s+"
-         },
-         "avatar_revision":1618002894086,
-         "banner_revision":1618610003550,
-         "bio":"**Hi I play osu**",
-         "friend_count":46
-      }
-   },
-   "cache":{
-      "status":"miss",
-      "cached_at":1633410893457,
-      "cached_until":1633411193457
-   }
-}
-
 
 const rankColors = {
   "D": "",
@@ -109,7 +53,7 @@ async function getUserData() {
 
 }
 async function updateData() {
-  let json = getUserData();
+  let json = await getUserData();
   console.log(json)
   let userRank = json.data.user.league.rank;
   rankImage.setAttribute("src", `src/ranks/` + userRank + `.png`);
