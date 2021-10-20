feb1bd2bc11b401d15bd0382581743a83eddf2c6
clxxiii <earthnoartisjusteh@gmail.com>
2021-10-20 09:42:50 -0400

    big update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const rankImage = document.getElementById("rankImage");
+const main = document.getElementById("main");
 const userName = document.getElementById("userName");
 const userTR = document.getElementById("userTR");
 const userProfile = document.getElementById("userProfile");
@@ -8,28 +9,29 @@ const API_URL = "https://7yortti0f2.execute-api.us-east-2.amazonaws.com/api?user
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const callTime = new Date().getTime();
-let testjson = {"success":true,"data":{"user":{"_id":"5e7a392fe55cb634a81b6127","username":"clxxiii","role":"user","ts":"2020-03-24T16:45:35.429Z","badges":[{"id":"secretgrade","label":"Achieved the full Secret Grade","ts":"2020-05-12T18:33:53.123Z"}],"xp":2112558,"gamesplayed":1875,"gameswon":539,"gametime":651885.3227955549,"country":"US","supporter":false,"supporter_tier":0,"verified":false,"league":{"gamesplayed":508,"gameswon":259,"rating":20424.231455449248,"glicko":1892.7417023783034,"rd":70.53631116493088,"rank":"s+","apm":34.17,"pps":1.45,"vs":75.09,"decaying":false,"standing":4616,"percentile":0.14529483990806913,"standing_local":1008,"prev_rank":"s","prev_at":5399,"next_rank":"ss","next_at":3494,"percentile_rank":"s+"},"avatar_revision":1618002894086,"banner_revision":1618610003550,"bio":"**Hi I play osu**","friend_count":46}},"cache":{"status":"hit","cached_at":1634709090901,"cached_until":1634709390901}}
+let testjson = {"success":true,"data":{"user":{"_id":"6076577db88e696ebeb54ba7","username":"themonkifier","role":"user","ts":"2021-04-14T02:46:21.716Z","badges":[],"xp":1086107,"gamesplayed":469,"gameswon":206,"gametime":202970.15600000013,"country":"US","supporter_tier":0,"verified":false,"league":{"gamesplayed":282,"gameswon":154,"rating":21448.868415521607,"glicko":1971.020337900401,"rd":62.39632916600284,"rank":"ss","apm":43.17,"pps":1.36,"vs":90.48,"decaying":false,"standing":3455,"percentile":0.10830302270161796,"standing_local":774,"prev_rank":"s+","prev_at":3508,"next_rank":"u","next_at":1595,"percentile_rank":"ss"},"avatar_revision":1621951855974,"friend_count":5}},"cache":{"status":"miss","cached_at":1634736913287,"cached_until":1634737213287}}
 
 const rankColors = {
-  "D": "",
-  "D+": "",
-  "C-": "",
-  "C": "",
-  "C+": "",
-  "B-": "",
-  "B": "",
-  "B+": "",
-  "A-": "",
-  "A": "",
-  "A+": "",
-  "S-": "",
-  "S": "",
-  "S+": "",
-  "SS": "",
-  "U": "",
-  "X": "",
-  "Z": "",
+  "d": "#856C84",
+  "d+": "#815880",
+  "c-": "#6C417C",
+  "c": "#67287B",
+  "c+": "#522278",
+  "b-": "#5949BE",
+  "b": "#4357B5",
+  "b+": "#4880B2",
+  "a-": "#35AA8C",
+  "a": "#3EA750",
+  "a+": "#43b536",
+  "s-": "#B79E2B",
+  "s": "#d19e26",
+  "s+": "#dbaf37",
+  "ss": "#e39d3b",
+  "u": "#c75c2e",
+  "x": "#b852bf",
+  "z": "#828282",
 };
+
 async function getUserData() {
   try {
     let currentJson = JSON.parse(localStorage.getItem("tetrioStatsUserData"))
@@ -61,13 +63,18 @@ async function getUserData() {
 
 async function updateData() {
   let json = await getUserData();
+
   try {
-  console.log(json)
+    console.log(json)
     let userRank = json.data.user.league.rank;
-    userProfile.setAttribute("src", "https://tetr.io/user-content/avatars/" + json.data.user._id + ".jpg?rv=" + json.data.user.avatar_revision )
+    main.style.background = rankColors[userRank]
+    if (!(json.data.user.avatar_revision == undefined)) {
+      userProfile.setAttribute("src", "https://tetr.io/user-content/avatars/" + json.data.user._id + ".jpg?rv=" + json.data.user.avatar_revision )
+    }
+    else { userProfile.style.width = 0; }
     rankImage.setAttribute("src", `src/ranks/` + userRank + `.png`);
     userName.innerHTML = json.data.user.username;
-    userTR.innerHTML = (Math.round(json.data.user.league.rating * 10) ) / 10;
+    userTR.innerHTML = (Math.round(json.data.user.league.rating * 10) ) / 10 + "<span id=subscript><sub>TR </sub></span>";
     globalRank.innerHTML = "#" + json.data.user.league.standing;
   }
   catch (error) {
